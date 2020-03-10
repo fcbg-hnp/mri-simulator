fbd47f6c6c2643718b29b6ddc4006ac88c2b32b1
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2020-03-10 17:47:45 +0100

    Adapt to params in ms

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+from os import path
 from time import sleep
 from pynput.keyboard import Key, Controller
 import configparser
@@ -6,16 +8,16 @@ import configparser
 def main(key_Type, repetition, period, waitingTime):
 
 	keyboard = Controller()
-	
+
 	# Waiting period before starting
 	waiting = divmod(waitingTime, 5)
 
 	# Every 10 secs 
-	for i in range(waiting[0]):
+	for i in range(int(waiting[0])):
 		t = i * 5
 		print("In waiting mode for: {} sec / {} sec".format(t, waitingTime))
 		sleep(waiting[0])
-	sleep(waiting[1]/1000)
+	sleep(waiting[1])
 
 	print("Starting the MRI simulation")
 
@@ -23,18 +25,29 @@ def main(key_Type, repetition, period, waitingTime):
 	for i in range(repetition):	
 		keyboard.press(key_Type)
 		keyboard.release(key_Type)
-		sleep(period/1000)
+		sleep(period)
 		
 
 if __name__ == '__main__':
 
+	if len(sys.argv) == 1:
+		config_file = './config.ini'
+	
+	elif len(sys.argv) == 2:
+		if path.isfile(sys.argv[1]):
+			config_file = sys.argv[1]
+		else:
+			raise IOError("Error: the provided file does not exist")
+	else:
+		raise IOError("Error: too many input arguments")
+
 	# Load the parameters
 	config = configparser.ConfigParser()
-	config.read('config.ini')
+	config.read(config_file)
 
 	key_Type = config['params']['key_Type']
 	repetition = int(config['params']['repetition'])
-	period = int(config['params']['period'])
-	waitingTime = int(config['params']['waitingTime'])
+	period = int(config['params']['period']) / 1000
+	waitingTime = int(config['params']['waitingTime']) / 1000
 
 	main(key_Type, repetition, period, waitingTime)
